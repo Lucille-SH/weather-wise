8a9a17a68aaf025d9bfda94c5ca0125d094b3813
Lucille-SH <145914878+Lucille-SH@users.noreply.github.com>
2023-11-25 19:16:09 +0000

    Added weather API.pt1
    main city and degrees data

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
+function updateWeatherData(response) {
+  console.log(response.data.temperature.current);
+
+  let mainDegrees = document.querySelector("#main-weather-degrees");
+  let temperature = response.data.temperature.current;
+  mainDegrees.innerHTML = Math.round(temperature);
+
+  let mainCity = document.querySelector("#main-city");
+  mainCity.innerHTML = response.data.city;
+}
+
+function refreshWeather(city) {
+  let apiKey = "66b4t441aodafc3797bfd80f9495a36b";
+  let url = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(url).then(updateWeatherData);
+}
+
 function handleCityClick(event) {
   event.preventDefault();
   let cityInput = document.querySelector("#city-search");
-  let updateCity = cityInput.value;
-  let mainCity = document.querySelector("#main-city");
-  mainCity.innerHTML = updateCity;
+
+  refreshWeather(cityInput.value);
 }
 
 let changeCity = document.querySelector("#city-submit");
 changeCity.addEventListener("click", handleCityClick);
-console.log(changeCity);
+
+refreshWeather("Dublin");
