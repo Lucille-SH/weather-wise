c5ed12049b515c114216aab12572966b6dfc990c
Lucille-SH <145914878+Lucille-SH@users.noreply.github.com>
2023-11-25 16:43:11 +0000

    Added basic search engine (no API)

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
+function handleCityClick(event) {
+  event.preventDefault();
+  let cityInput = document.querySelector("#city-search");
+  let updateCity = cityInput.value;
+  let mainCity = document.querySelector("#main-city");
+  mainCity.innerHTML = updateCity;
+}
 
+let changeCity = document.querySelector("#city-submit");
+changeCity.addEventListener("click", handleCityClick);
+console.log(changeCity);
