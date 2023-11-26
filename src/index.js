07cf64aed3f539720ba865ff155f3dcd20eae88a
Lucille-SH <145914878+Lucille-SH@users.noreply.github.com>
2023-11-26 14:48:44 +0000

    Added search engine weather data

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
 function updateWeatherData(response) {
-  console.log(response.data.temperature.current);
-
   let mainDegrees = document.querySelector("#main-weather-degrees");
-  let temperature = response.data.temperature.current;
-  mainDegrees.innerHTML = Math.round(temperature);
-
   let mainCity = document.querySelector("#main-city");
+  let humidityElement = document.querySelector("#humidity-percent");
+  let windElement = document.querySelector("#wind-kms");
+  let conditionElement = document.querySelector("#current-condition");
+  let dayTimeElement = document.querySelector("#current-day-time");
+  let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#main-weather-icon");
+
   mainCity.innerHTML = response.data.city;
+  mainDegrees.innerHTML = Math.round(response.data.temperature.current);
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windElement.innerHTML = `${response.data.wind.speed}km/h`;
+  conditionElement.innerHTML = response.data.condition.description;
+  dayTimeElement.innerHTML = formatDate(date);
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="main-weather-icon">`;
+}
+
+function formatDate(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 
 function refreshWeather(city) {
